4d0033baf82fff3d59096b96b011e3b9cc3ab2b3
JainamKarania <jainamkarania05@gmail.com>
2026-09-17 12:09:42 +0530

    Removed Booking.jsx and dead Component

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
-import Booking from '../Components/Booking'
 import ContactUs from '../Components/ContactUs'
 import Footer from '../Components/Footer'
 import BookingsCTA from '../Components/BookingsCTA'
